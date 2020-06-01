f5e9a74c9fdd222a58346055ab7581b55527e21a
Hammed Oyedele <itz.harmid@gmail.com>
2020-06-01 16:39:18 +0100

    style: format the js with prettier

--- src/index.js
+++ src/index.js
@@ -1,74 +1,84 @@
 import React from 'react'
-import ReactDOM from 'react-dom'
+import { render } from 'react-dom'
 
-function CountDown({ hours = 0, minutes = 0, seconds = 0 }) {
-  const [paused, setPaused] = React.useState(false);
-  const [over, setOver] = React.useState(false);
+function CountDown ({ hours = 0, minutes = 0, seconds = 0 }) {
+  const [paused, setPaused] = React.useState(false)
+  const [over, setOver] = React.useState(false)
   const [time, setTime] = React.useState({
     hours: parseInt(hours),
     minutes: parseInt(minutes),
     seconds: parseInt(seconds)
-  });
+  })
 
   const tick = () => {
-    if (paused || over) return;
-    if (time.hours == 0 && time.minutes == 0 && time.seconds == 0) setOver(true);
-    else if (time.minutes == 0 && time.seconds == 0)
+    if (paused || over) return
+    if (time.hours === 0 && time.minutes === 0 && time.seconds === 0) {
+      setOver(true)
+    } else if (time.minutes === 0 && time.seconds === 0) {
       setTime({
         hours: time.hours - 1,
         minutes: 59,
         seconds: 59
-      });
-    else if (time.seconds == 0)
+      })
+    } else if (time.seconds === 0) {
       setTime({
         hours: time.hours,
         minutes: time.minutes - 1,
         seconds: 59
-      });
-    else
+      })
+    } else {
       setTime({
         hours: time.hours,
         minutes: time.minutes,
         seconds: time.seconds - 1
-      });
-  };
+      })
+    }
+  }
 
   const reset = () => {
     setTime({
       hours: parseInt(hours),
       minutes: parseInt(minutes),
       seconds: parseInt(seconds)
-    });
-    setPaused(false);
-    setOver(false);
-  };
+    })
+    setPaused(false)
+    setOver(false)
+  }
 
   React.useEffect(() => {
-    let timerID = setInterval(() => tick(), 1000);
-    return () => clearInterval(timerID);
-  });
+    const timerID = setInterval(() => tick(), 1000)
+    return () => clearInterval(timerID)
+  })
 
   return (
     <div>
-      <p>{`${time.hours.toString().padStart(2, '0')}:${time.minutes
-        .toString()
-        .padStart(2, '0')}:${time.seconds.toString().padStart(2, '0')}`}</p>
+      <p>
+        {`${time.hours
+          .toString()
+          .padStart(2, '0')}:${time.minutes
+          .toString()
+          .padStart(2, '0')}:${time.seconds.toString().padStart(2, '0')}`}
+      </p>
       <div>{over ? "Time's up!" : ''}</div>
-      <button onClick={() => setPaused(!paused)}>{paused ? 'Resume' : 'Pause'}</button>
+      <button onClick={() => setPaused(!paused)}>
+        {paused ? 'Resume' : 'Pause'}
+      </button>
       <button onClick={() => reset()}>Restart</button>
     </div>
-  );
+  )
 }
 
-const containers = [].slice.call(document.querySelectorAll('.react-countdown'))
+const reactCountdownElements = [].slice.call(
+  document.querySelectorAll('.react-countdown')
+)
 
-containers.forEach((container) => {
-  const hours = container.getAttribute('data-hours') ?? 0
-  const minutes = container.getAttribute('data-minutes') ?? 0
-  const seconds = container.getAttribute('data-seconds') ?? 0
+reactCountdownElements.forEach((reactCountdownElement) => {
+  const hours = reactCountdownElement.getAttribute('data-hours') ?? 0
+  const minutes = reactCountdownElement.getAttribute('data-minutes') ?? 0
+  const seconds = reactCountdownElement.getAttribute('data-seconds') ?? 0
 
-  ReactDOM.render(
+  render(
     <CountDown hours={hours} minutes={minutes} seconds={seconds} />,
-    container
+    reactCountdownElement
   )
 })
